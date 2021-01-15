18768e665c9119ac53d5e94da3b6ed7cb8ed812e
manochithra92 <77476690+manochithra92@users.noreply.github.com>
2021-01-14 21:07:22 -0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -10,7 +10,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 m<<-NULL
         }
         get <- function()x
-        setinverse<-function(inverse)i<<-inverse
+        setinverse<-function(inverse)m<<-inverse
         getinverse<function()m
         list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
